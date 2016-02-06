6d95adf626767b2782e4f30dc66246ba24551950
Dimitrios Liappis <dimitrios.liappis@gmail.com>
2016-02-06 11:58:12 +0200

    Setting Fira-Mono as the default font
    Reveritng back to zenburn theme (default)

---
+++ personal/preload/dimitris.el
@@ -0,0 +1,7 @@
+;;; Preload personalized stuff here
+
+;;; Code:
+(setq prelude-theme 'zenburn)
+(set-frame-font "-CTDB-Fira Mono-normal-normal-normal-*-*-*-*-*-m-0-iso10646-1")
+(provide 'dimitris)
+;;; dimitris.el ends here
