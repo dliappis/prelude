c6524fc8a574fc4885e2b2c88dc61dd078273ce3
Dimitrios Liappis <dl@localhost.localdomain>
2018-05-19 21:27:16 +0300

    Set js-indent-level
    and monkey patch projectile discovery

--- personal/dimitris.el
+++ personal/dimitris.el
@@ -192,5 +192,27 @@
              "* %?\nEntered on %U\n  %i\n  %a")))
 (define-key global-map "\C-cc" 'org-capture)
 
+;; js-mode
+
+(setq js-indent-level 2)
+
+;; monkey patch discover prjectile projects
+(defun projectile-discover-projects-in-directory (directory)
+  "Discover any projects in DIRECTORY and add them to the projectile cache.
+This function is not recursive and only adds projects with roots
+at the top level of DIRECTORY."
+  (interactive
+   (list (read-directory-name "Starting directory: ")))
+  (let ((subdirs (directory-files directory t)))
+    (mapcar
+     (lambda (dir)
+       (when (and (file-directory-p dir)
+                  (not (member (file-name-nondirectory dir) '(".." "."))))
+         (let ((default-directory dir)
+               (projectile-cached-project-root dir))
+           (when (projectile-project-p)
+             (projectile-add-known-project (projectile-project-root))))))
+     subdirs)))
+
 (provide 'dimitris)
 ;;; dimitris.el ends here
